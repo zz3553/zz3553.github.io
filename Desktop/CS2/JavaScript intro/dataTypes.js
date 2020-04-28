01a6bad34f01e306df1d429f0af20927eb026425
Zaarib <zz3553@g.rit.edu>
2020-04-28 18:19:01 -0400

    Practicing with data types

--- Desktop/CS2/JavaScript intro/dataTypes.js
+++ Desktop/CS2/JavaScript intro/dataTypes.js
@@ -1,2 +1,28 @@
 // Working with data types
 
+var x = 16 + "hi"
+var y = true
+
+console.log(x==y)
+
+var x = 5e3
+var y = 5e-3
+
+console.log(x + " " + y)
+
+var array = [16, 1, 'a', 'String']
+
+console.log(array[0])
+
+var song = {title:"Antidote", artist: "Travis Scott", releaseData: 2015}
+
+console.log(song.title)
+
+console.log(typeof "")
+console.log(typeof 16)
+console.log(typeof array)
+console.log(typeof song)
+
+var undef
+
+console.log(undef)
