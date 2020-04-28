6547a525b34e54d122fc943d558cc02fee49b2f7
Zaarib <zz3553@g.rit.edu>
2020-04-28 19:13:27 -0400

    Functions...

--- Desktop/CS2/JavaScript intro/funct.js
+++ Desktop/CS2/JavaScript intro/funct.js
@@ -1,1 +1,15 @@
 // testing functions 
+
+function testing() {
+    return "testing..."
+}
+
+console.log(testing())
+
+function simpleMath(num1, num2) {
+    return num1 + num2
+}
+
+var x = simpleMath(1,2)
+
+console.log(x)
