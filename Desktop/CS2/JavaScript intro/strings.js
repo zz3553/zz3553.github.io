6ac8d3a834d06af0954a47c0d1fa3ba927296a54
Zaarib <zz3553@g.rit.edu>
2020-04-28 22:52:03 -0400

    Practiced with strings

--- Desktop/CS2/JavaScript intro/strings.js
+++ Desktop/CS2/JavaScript intro/strings.js
@@ -1,1 +1,12 @@
-// testing strings
+// testing strings and their properties
+
+var a = "Hey I'm\
+ Mitchel"
+var lena = a.length
+
+console.log(lena)
+console.log(a)
+
+var b = 'Some people call me \n\"Zaarib" '
+
+console.log(b)
