6d44305f7a38d2c8091cf753f588c19d4bf64d9e
Zaarib <zz3553@g.rit.edu>
2020-04-29 19:43:01 -0400

    More practice with substrings and indexing

--- Desktop/CS2/JavaScript intro/strings.js
+++ Desktop/CS2/JavaScript intro/strings.js
@@ -8,5 +8,11 @@ console.log(lena)
 console.log(a)
 
 var b = 'Some people call me \n\"Zaarib" '
-
 console.log(b)
+
+var index = b.indexOf('some')
+console.log("index of \'some' " + index)
+
+console.log("index of call " + b.search("call"))
+console.log(b.slice(5,11)) //uses index as second parameter 
+console.log(b.substr(5,6)) //uses length as second parameter 
