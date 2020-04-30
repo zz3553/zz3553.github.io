ef0adad48996b8ac37bae6003c8c74bf93b22845
Zaarib <zz3553@g.rit.edu>
2020-04-29 22:27:44 -0400

    Practicing forEach method

--- Desktop/CS2/JavaScript intro/arrays.js
+++ Desktop/CS2/JavaScript intro/arrays.js
@@ -8,5 +8,20 @@ function funct(artist) {
     return artist
 }
 
-console.log(artists.forEach(funct)) //doing somethign with each element
+var num = [1,2,3,4,5,6,7]
+var newNums = new Array()
+num.forEach(add)
+
+function add(num) {
+    newNums.push(num+30)
+}
+
+num.forEach(iter)
+function iter(num, i, arr) {
+    arr[i] = num + 5
+}
+
+console.log(num)
+
+console.log(newNums)
 
