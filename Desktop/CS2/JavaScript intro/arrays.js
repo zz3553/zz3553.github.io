5a6b0b891eddf26865d289012f22de3a6f3385ce
Zaarib <zz3553@g.rit.edu>
2020-04-29 22:19:22 -0400

    Pushing array work

--- Desktop/CS2/JavaScript intro/arrays.js
+++ Desktop/CS2/JavaScript intro/arrays.js
@@ -1,1 +1,12 @@
 // testing arrays in js
+
+var artists = ["Travis Scott", "Kanye West", "Iann Dior", "Lil Uzi"]
+
+
+function funct(artist) {
+    artist = "1" + artist
+    return artist
+}
+
+console.log(artists.forEach(funct)) //doing somethign with each element
+
